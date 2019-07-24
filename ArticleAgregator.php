9b33d180774622780a6530cb5d8a0c3335aa2888
valentin <perot.valentin@gmail.com>
2019-07-24 17:04:45 +0200

    Add ArtcleAgregator for test

---
+++ ArticleAgregator.php
@@ -0,0 +1,116 @@
+<?php
+
+class ArticleAgregator
+{
+    protected $articles = [];
+
+    protected function createTables($pdo)
+    {
+        $query = "CREATE TABLE IF NOT EXISTS source (
+            id int NOT NULL auto_increment,
+            name varchar(255),
+            PRIMARY KEY(id)
+            );
+            CREATE TABLE IF NOT EXISTS article (
+                id int NOT NULL auto_increment,
+                source_id int NOT NULL,
+                name varchar(255),
+                content BLOB,
+                PRIMARY KEY(id)
+            );
+            INSERT INTO source VALUES (1, 'src-1');
+            INSERT INTO source VALUES (2, 'src-2');
+            INSERT INTO article VALUES (1, 1, 'Article 1', 'Lorem ipsum dolor sit amet 1');
+            INSERT INTO article VALUES (2, 2, 'Article 2', 'Lorem ipsum dolor sit amet 2');
+            INSERT INTO article VALUES (3, 2, 'Article 3', 'Lorem ipsum dolor sit amet 3');
+            INSERT INTO article VALUES (4, 1, 'Article 4', 'Lorem ipsum dolor sit amet 4');"
+        ;
+
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+    }
+
+    public function appendDatabase($port, $username, $password, $database)
+    {
+        // En partant de l'idée que la database alltricks_test existe déjà ! :)
+        $dsn = "mysql:host={$port};dbname={$database}";
+        try {
+            $pdo = new PDO($dsn, $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo 'Connexion échoué : ' . $e->getMessage();
+            exit();
+        }
+
+        $this->createTables($pdo);
+
+        $querySelectProducts = "SELECT 
+            article.name, 
+            source.name AS sourceName, 
+            content 
+            FROM article 
+            INNER JOIN source 
+            ON source.id = article.source_id"
+        ;
+
+        $stmt = $pdo->prepare($querySelectProducts);
+        $stmt->execute();
+
+        while($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $article = new \stdClass();
+
+            $article->name = $row["name"];
+            $article->sourceName = $row["sourceName"];
+            $article->content = $row["content"];
+
+            $this->articles[] = $article;
+        }
+    }
+
+    public function appendRss($name, $feedUrl)
+    {
+        // Get the content of url and instantiate in SimpleXMlElement
+        $content = file_get_contents($feedUrl);
+        $flux = new \SimpleXmlElement($content);
+        
+        foreach($flux->channel->item as $item) {
+            $article = new \stdClass();
+            
+            $article->name = $item->title;
+            $article->sourceName = $name;
+            $article->content = $item->link;
+
+            $this->articles[] = $article;
+        }
+    }
+
+    public function getArticles()
+    {
+        return $this->articles;
+    }
+}
+
+$a = new ArticleAgregator();
+
+/**
+ * Récupère les articles de la base de données, avec leur source.
+ * host, username, password, database name
+ * 
+ * password = local personnal password
+ */
+$a->appendDatabase('localhost', 'root', '', 'alltricks_test');
+
+/**
+ * Récupère les articles d'un flux rss donné
+ * source name, feed url
+ */
+$a->appendRss('Le Parisien', 'http://www.leparisien.fr/actualites-a-la-une.rss.xml');
+$a->appendRss('Le Monde',    'http://www.lemonde.fr/rss/une.xml');
+
+foreach ($a->getArticles() as $article) {
+    echo sprintf("<h2>%s</h2>'\n'<em>%s</em>'\n'<p>%s</p>'\n'",
+        $article->name,
+        $article->sourceName,
+        $article->content
+    );
+}
